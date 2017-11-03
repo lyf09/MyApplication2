3608dda8d24fb520651111b4fc83690a941cdea8
lyf09 <2331692234@qq.com>
2017-11-03 20:38:16 +0800

    first

---
+++ app/src/main/java/nuaa/testnetwork/SignupActivity.java
@@ -0,0 +1,187 @@
+package nuaa.testnetwork;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.administrator.myapplication.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.Bind;
+import nuaa.util.NetworkUtils;
+
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    @Bind(R.id.input_xuehao)
+    EditText _nameText;
+    @Bind(R.id.input_email)
+    EditText _emailText;
+    @Bind(R.id.input_password)
+    EditText _passwordText;
+    @Bind(R.id.btn_signup)
+    Button _signupButton;
+    @Bind(R.id.link_login)
+    TextView _loginLink;
+
+
+    private EditText input_xuehao;
+    private EditText input_password;
+    private EditText input_email;
+    private Button submit;
+    private Handler handler;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+//        ButterKnife.inject(this);
+
+        input_xuehao = (EditText) findViewById(R.id.input_xuehao);
+        input_password = (EditText) findViewById(R.id.input_password);
+        input_email = (EditText) findViewById(R.id.input_email);
+        submit = (Button) findViewById(R.id.btn_signup);
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                input_xuehao.setText("");
+                input_email.setText("");
+                input_password.setText("");
+                Toast.makeText(SignupActivity.this, "数据插入成功", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread t = new Thread() {
+                    @Override
+                    public void run() {
+
+                        Map<String, String> map = new HashMap<>();
+                        map.put("id", input_xuehao.getText().toString());
+                        map.put("pwd", input_email.getText().toString());
+                        map.put("email", input_password.getText().toString());
+                        try {
+                            String str = NetworkUtils.postDataByURL(NetworkUtils.INSERT_STU_URL, map);
+                            System.out.println(str + "---------");
+                            handler.sendEmptyMessage(0);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
+            }
+        });
+
+//物理格式控制
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.Theme_AppCompat_DayNight_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
+}
+
---
+++ app/src/main/java/nuaa/testnetwork/SplashActivity.java
@@ -0,0 +1,31 @@
+package nuaa.testnetwork;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.myapplication.LoginActivity;
+import com.example.administrator.myapplication.R;
+
+/**
+ * Created by Administrator on 2017/10/28.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //延迟2S跳转
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
