eb66887bf365b0d45368a3fda754731fc8afc7e8
lyf09 <2331692234@qq.com>
2017-11-03 21:24:44 +0800

    first

--- app/src/main/java/nuaa/testnetwork/SplashActivity.java
+++ app/src/main/java/nuaa/testnetwork/SplashActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.administrator.myapplication.LoginActivity;
 import com.example.administrator.myapplication.R;
 
 /**
